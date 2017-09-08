5349ba306b28b44114d9437acfc657db4482eec9
Vasiliy <gladishevvasiliy@gmail.com>
2017-09-08 19:35:39 +0300

    evening comit

---
+++ 7/script.js
@@ -0,0 +1,15 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+
+function showOk() {
+  alert( "Вы согласились." );
+}
+
+function showCancel() {
+  alert( "Вы отменили выполнение." );
+}
+
+// использование
+ask("Вы согласны?", showOk, showCancel);
