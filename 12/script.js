497d283f4d7c752740b368c265c91cdce5b9fb0c
Vasiliy <gladishevvasiliy@gmail.com>
2017-09-15 22:01:36 +0300

    max salary

--- 12/script.js
+++ 12/script.js
@@ -1,4 +1,5 @@
-function isEmpty(obj) {
+// Exersize 1
+/*function isEmpty(obj) {
   var counter = 0;
   for (var key in obj) {
     return false;
@@ -13,3 +14,37 @@ alert( isEmpty(schedule) ); // true
 schedule["8:30"] = "подъём";
 
 alert( isEmpty(schedule) ); // false
+*/
+
+// Exersize 2
+
+/*var salaries = {
+  "Вася": 100,
+  "Петя": 300,
+  "Даша": 250
+};
+
+var sum = 0;
+for (var key in salaries) {
+  sum += salaries[key];
+}
+alert( sum );
+*/
+
+// Exersize 3
+
+var salaries = {
+  "Вася": 100,
+  "Петя": 300,
+  "Даша": 250
+};
+maxSalary = 0;
+nameMax = ''
+for (var key in salaries) {
+  if (salaries[key] > maxSalary) {
+    maxSalary = salaries[key];
+    nameMax = key;
+  }
+}
+
+alert(nameMax);
