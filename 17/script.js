08eb0c3631f1d643573b4760b1af1690f1da20a8
Vasiliy <gladishevvasiliy@gmail.com>
2017-10-03 20:05:52 +0300

    before oop commit

--- 17/script.js
+++ 17/script.js
@@ -235,23 +235,22 @@ for (var i = 0; i < log.length; i++) {
 
 */
 
+/*
+
 function f(x) {
   return Math.random() * x; // random для удобства тестирования
 }
 
 function makeCaching(f) {
-  var last, result;
+  var cache = {};
 
-  function wrapper(a) {
-    if (a == last){
-      return result;
+  return function(x) {
+    if (!(x in cache)) {
+      cache[x] = f.call(this, x);
     }
-    last = a;
-    result = f.call(this, a);
-    return result;
+    return cache[x];
+  };
 
-  }
-  return wrapper;
 }
 
 f = makeCaching(f);
@@ -264,3 +263,182 @@ alert( a == b ); // true (значение закешировано)
 
 b = f(2);
 alert( a == b ); // false, другой аргумент => другое значение
+
+*/
+
+/*
+function formatDate(date) {
+  if (typeof date == 'string') {
+      var arr = date.split('-');
+      return arr[2] + '.' + arr[1] + '.' + arr[0].slice(2);
+  }
+}
+
+alert( formatDate('2011-10-02') ); // 02.10.11
+*/
+
+/*
+var leader = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+var str = JSON.stringify(leader);
+var newLeader = JSON.parse(str);
+*/
+
+/*
+var leader = {
+  name: "Василий Иванович"
+};
+
+var soldier = {
+  name: "Петька"
+};
+
+// эти объекты ссылаются друг на друга!
+leader.soldier = soldier;
+soldier.leader = leader;
+
+var team = [leader, soldier];
+
+var str = JSON.stringify(team);
+alert(str);
+*/
+
+
+
+/*
+printNumbersInterval();
+
+function printNumbersInterval() {
+  var i = 1;
+  var timerId = setInterval(function() {
+    console.log(i);
+    if (i == 20) {
+      clearTimeout(timerId);
+    }
+    i++;
+  }, 100);
+}
+
+*/
+/*
+printNumbersInterval(1);
+
+function printNumbersInterval(i){
+  var timerId = setTimeout(function count() {
+    console.log(i);
+    if (i < 20) {
+      setTimeout(count, 100);
+    }
+    i++;
+  }, 100);
+}
+*/
+
+/*
+
+function f(x) {
+  alert( x );
+}
+
+var f1000 = delay(f, 1000);
+var f1500 = delay(f, 1500);
+
+f1000("тест"); // выведет "тест" через 1000 миллисекунд
+f1500("тест2"); // выведет "тест2" через 1500 миллисекунд
+
+function delay(f, ms) {
+  return function () {
+    savedThis = this;
+    savedArgs = arguments;
+
+    setTimeout(function() {
+      f.apply(savedThis, savedArgs);
+    }, ms);
+  }
+}
+
+*/
+
+
+/*
+function f(x) {alert(x)}
+
+var f = debounce(f, 1000);
+
+f(1); // выполнится сразу же
+f(2); // игнор
+
+setTimeout( function() { f(3) }, 100); // игнор (прошло только 100 мс)
+setTimeout( function() { f(4) }, 1100); // выполнится
+setTimeout( function() { f(5) }, 1500); // игнор
+
+function debounce(f, ms) {
+  var state = null;
+  COOLDOWN = 1;
+
+  return function () {
+
+    if (state){
+        return;
+    }
+    f.apply(this, arguments);
+
+    state = COOLDOWN;
+    setTimeout(function () {
+      state = null;
+    }, ms);
+  }
+}
+
+*/
+
+/*
+
+var f = function(a) {
+  alert(a);
+};
+
+// затормозить функцию до одного раза в 1000 мс
+var f1000 = throttle(f, 1000);
+
+f1000(1); // выведет 1
+f1000(2); // (тормозим, не прошло 1000 мс)
+f1000(3); // (тормозим, не прошло 1000 мс)
+
+// когда пройдёт 1000 мс...
+// выведет 3, промежуточное значение 2 игнорируется
+
+function throttle(func, ms) {
+  var isThorttled = false,
+  savedThis,
+  savedArgs;
+
+  function wrapper() {
+
+    if (isThorttled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThorttled = true;
+
+    setTimeout(function () {
+      isThorttled = false;
+      if (savedArgs){
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+  return wrapper;
+}
+*/
+
+str = prompt();
+alert( eval(str) );
