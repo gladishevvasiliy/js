f6f1e22d8f2e1b55a8e6afd74b9f6e975a547650
Vasiliy <gladishevvasiliy@gmail.com>
2017-10-05 23:57:50 +0300

    err commit

--- 19/script.js
+++ 19/script.js
@@ -1,5 +1,5 @@
 
-function CoffeeMachine(power) {
+/*function CoffeeMachine(power) {
   this._waterAmount = 0;
   this._power = power;
 }
@@ -24,3 +24,30 @@ CoffeeMachine.prototype.setWaterAmount = function(amount) {
 var coffeeMachine = new CoffeeMachine(10000);
 coffeeMachine.setWaterAmount(50);
 coffeeMachine.run();
+
+*/
+
+
+function FormatError(message) {
+  SyntaxError.apply(this, arguments);
+  this.name = "FormatError";
+  this.message = message;
+
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(this, this.constructor);
+  } else {
+    this.stack = (new Error()).stack;
+  }
+
+}
+
+FormatError.prototype = Object.create(SyntaxError.prototype);
+FormatError.prototype.constructor = FormatError;
+
+var err = new FormatError("ошибка форматирования");
+
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // стек на момент генерации ошибки
+
+alert( err instanceof SyntaxError ); // true
